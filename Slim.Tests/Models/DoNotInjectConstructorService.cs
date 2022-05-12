dd1b589b5d41b3a79f45e505860a8e8d80cd5166
Macocian Alexandru Victor <amacocian@yahoo.com>
2022-05-12 18:13:28 +0200

    Implement DoNotInject attribute (#11)
    * Implement DoNotInject attribute
    
    * Upgrade pipelines

---
+++ Slim.Tests/Models/DoNotInjectConstructorService.cs
@@ -0,0 +1,12 @@
+﻿using Slim.Attributes;
+
+namespace Slim.Tests.Models
+{
+    public sealed class DoNotInjectConstructorService
+    {
+        [DoNotInject]
+        public DoNotInjectConstructorService()
+        {
+        }
+    }
+}
